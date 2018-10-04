fcfe4fb149f4754a9318e7fb7d4400cd55513ea1
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-10-04 08:39:31 +0700

    fix UI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
