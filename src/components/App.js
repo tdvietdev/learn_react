a1cafcb344065095fbbcd85dcbdea197e0318b24
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-10-05 09:33:34 +0700

    use link

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,19 @@ import ListNews from "./news";
 import Contact from "./contacs";
 import Post from "./post";
 import Redirect from "./route/redirect";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div>
         <Nav/>
         <Header/>
         <Redirect/>
         <Footer/>
       </div>
+      </BrowserRouter>
     );
   }
 }
