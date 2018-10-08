d4a2a60ad20c253831027968c180e315e6d43985
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-10-08 14:57:53 +0700

    use link-router

--- src/components/route/redirect.js
+++ src/components/route/redirect.js
@@ -8,9 +8,9 @@ class Redirect extends Component {
   render() {
     return (
 <div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/news" component={ListNews} />
+  <Route exact path="/" component={Home} />
+  <Route exact path="/contact" component={Contact} />
+  <Route exact path="/news" component={ListNews} />
 </div>
     );
   }
